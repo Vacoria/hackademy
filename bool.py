bc3f106333910ddf375a4f39ebc4deb333b8456b
Valentina Coria <vacoria@microsoft.com>
2019-02-14 09:32:58 +0000

    add bool.py to git

---
+++ bool.py
@@ -0,0 +1,20 @@
+#tip: boolean elements is true or false. you can use operators
+# and (gives true if both are true), or (gives true if at least one is true), not (gives the opposite)
+
+a_bool = True
+another_bool = False
+
+if a_bool: 
+   print("wow it was actually true!")
+
+a= 23
+b = 23
+
+if a<b: #it is called the true branch
+    print(" you know what? it was true! ")
+else: # it is called the else branch
+    print("it was actually false :( ")
+
+print("done")
+
+#to check if two numbers are equal, use ==
